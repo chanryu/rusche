58f7ebaee3e5f089ee555fd28ae3a8d96341987e
Chan Ryu <chan.ryu@safetyculture.io>
2024-08-19 13:00:18 +1000

    wip

--- src/scanner.rs
+++ src/scanner.rs
@@ -80,15 +80,15 @@ where
     fn read_string(&mut self) -> ScanResult {
         let mut text = String::new();
         let mut escaped = false;
-        while let Some(ch) = self.iter.next() {
-            match ch {
-                _ if escaped => {
+        for ch in &mut self.iter {
+            match (ch, escaped) {
+                (ch, true) => {
                     escaped = false;
                     text.push(ch);
                 }
-                '\\' => escaped = true,
-                '"' => return Ok(Token::Str(text)),
-                _ => text.push(ch),
+                ('"', false) => return Ok(Token::Str(text)),
+                ('\\', false) => escaped = true,
+                (ch, false) => text.push(ch),
             }
         }
         Err(ScanError::IncompleteString)
@@ -96,19 +96,23 @@ where
 
     fn read_number(&mut self, first_char: char) -> ScanResult {
         let mut has_decimal_point = false;
-        let mut digits = first_char.to_string();
+        let mut digits = String::new();
+
+        digits.push(first_char);
         while let Some(ch) = self
             .iter
             .next_if(|&ch| ch.is_ascii_digit() || (!has_decimal_point && ch == '.'))
         {
             digits.push(ch);
-            has_decimal_point |= ch == '.';
+            if ch == '.' {
+                has_decimal_point = true;
+            }
         }
-        if let Ok(value) = digits.parse::<f64>() {
-            Ok(Token::Num(value))
-        } else {
-            Err(ScanError::InvalidNumber)
-        }
+
+        digits
+            .parse::<f64>()
+            .map(|value| Ok(Token::Num(value)))
+            .unwrap_or_else(|_| Err(ScanError::InvalidNumber))
     }
 
     fn read_symbol(&mut self, first_char: char) -> ScanResult {
