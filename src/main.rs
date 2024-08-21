2820eac371ac782844dc54041510fcbf56b3b0bb
Chan Ryu <chan.ryu@safetyculture.io>
2024-08-21 19:08:01 +1000

    wip

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,7 @@ use eval::eval;
 use parser::Parser;
 
 fn main() {
-    //let mut parser = Parser::new("(+ 1 (* 2 4))".chars());
-    let mut parser = Parser::new("(quote (1 (* 2 4)) 1)".chars());
+    let mut parser = Parser::new("(+ 1 (* 2 4))".chars());
     if let Ok(expr) = parser.parse() {
         let env = Env::new_root_env();
         match eval(&expr, &env) {
