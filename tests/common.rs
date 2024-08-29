a4b10426eb24c47c150247c7078486337d1dd6b2
Chan Ryu <chan.ryu@safetyculture.io>
2024-08-29 18:31:19 +1000

    wip

--- tests/common.rs
+++ tests/common.rs
@@ -29,9 +29,12 @@ pub fn parse_expr(text: &str) -> Expr {
     }
 }
 
-pub fn eval_expr(expr: &str) -> EvalResult {
+pub fn eval_expr(expr: &str) -> String {
     let env = Env::new_root_env();
     env.set("t", Expr::Sym("#t".into()));
     env.set("f", NIL);
-    eval(&parse_expr(expr), &env)
+    match eval(&parse_expr(expr), &env) {
+        Ok(expr) => expr.to_string(),
+        Err(error) => format!("Err: {}", error),
+    }
 }
--- tests/primitives.rs
+++ tests/primitives.rs
@@ -1,28 +1,21 @@
 mod common;
 
-#[macro_use]
-extern crate rusp;
-
 use common::eval_expr;
-use rusp::{
-    expr::shortcuts::num,
-    list::{cons, List},
-};
 
 #[test]
 fn test_cond() {
-    assert_eq!(eval_expr("(cond (t 0) (f 1))"), Ok(num(0)));
-    assert_eq!(eval_expr("(cond (f 0) (t 1))"), Ok(num(1)));
+    assert_eq!(eval_expr("(cond (t 0) (f 1))"), "0");
+    assert_eq!(eval_expr("(cond (f 0) (t 1))"), "1");
 }
 
 #[test]
 fn test_lambda() {
-    assert_eq!(eval_expr("((lambda (x) (* x 2)) 5)"), Ok(num(10)));
+    assert_eq!(eval_expr("((lambda (x) (* x 2)) 5)"), "0");
 }
 
 #[test]
 fn test_quote() {
-    assert_eq!(eval_expr("'1"), Ok(num(1)));
-    assert_eq!(eval_expr("'(1)"), Ok(list!(num(1)).into()));
-    assert_eq!(eval_expr("'(1 2)"), Ok(list!(num(1), num(2)).into()));
+    assert_eq!(eval_expr("'1"), "1");
+    assert_eq!(eval_expr("'(1)"), "(1)");
+    assert_eq!(eval_expr("'(1 2)"), "(1 2)");
 }
