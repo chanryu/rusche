a98598860fc4b51500e1c38ab14af77d97d6983d
Chan Ryu <chan.ryu@safetyculture.io>
2024-09-12 21:53:29 +1000

    wip

--- src/bin/rusp/run_file.rs
+++ src/bin/rusp/run_file.rs
@@ -7,40 +7,46 @@ use rusp::{
 
 pub fn run_file(file_path: &str) {
     match std::fs::read_to_string(file_path) {
-        Ok(contents) => match tokenize(&contents) {
-            Ok(tokens) => {
-                let env = Env::new_root_env();
-
-                let mut parser = Parser::new();
-                parser.add_tokens(tokens);
-                loop {
-                    match parser.parse() {
-                        Ok(expr) => match eval(&expr, &env) {
-                            Ok(_) => {}
-                            Err(e) => {
-                                eprintln!("Error: {}", e);
-                                std::process::exit(1);
-                            }
-                        },
-                        Err(ParseError::NeedMoreToken) => break,
-                        Err(e) => {
-                            eprintln!("Parse Error: {}", e);
-                            std::process::exit(1);
-                        }
-                    }
-                }
-                if parser.is_parsing() {
-                    eprintln!("Unexpected end of file.");
-                    std::process::exit(1);
-                }
+        Ok(contents) => {
+            if let Err(e) = run_file_contents(&contents) {
+                eprintln!("{}", e);
+                std::process::exit(1);
             }
-            Err(e) => {
-                println!("Tokenize Error: {}", e);
-            }
-        },
+        }
         Err(e) => {
-            eprintln!("Error reading file: {}", e);
+            eprintln!("{}", e);
             std::process::exit(1);
         }
+    }
+}
+
+fn run_file_contents(text: &str) -> Result<(), String> {
+    match tokenize(&text) {
+        Ok(tokens) => {
+            let env = Env::new_root_env();
+
+            let mut parser = Parser::new();
+            parser.add_tokens(tokens);
+            loop {
+                match parser.parse() {
+                    Ok(expr) => match eval(&expr, &env) {
+                        Ok(_) => {}
+                        Err(e) => {
+                            return Err(format!("{}", e));
+                        }
+                    },
+                    Err(ParseError::NeedMoreToken) => break,
+                    Err(e) => {
+                        return Err(format!("{}", e));
+                    }
+                }
+            }
+            if parser.is_parsing() {
+                Err("Unexpected end of file.".to_owned())
+            } else {
+                Ok(())
+            }
+        }
+        Err(e) => Err(format!("{}", e)),
     }
 }
