f8bafefd91df70bfbff9d45d90a30de330f74cb3
Chan Ryu <chan.ryu@safetyculture.io>
2024-09-20 11:51:55 +1000

    More I/O functions - read, print (revised) (#40)

--- src/bin/rusp/prelude/native.rs
+++ src/bin/rusp/prelude/native.rs
@@ -1,6 +1,7 @@
-use std::rc::Rc;
+use std::{io::Write, rc::Rc};
 
 use rusp::{
+    builtin::utils::{eval_to_str, get_exact_1_arg},
     env::Env,
     eval::{eval, EvalResult},
     expr::{Expr, NIL},
@@ -8,14 +9,53 @@ use rusp::{
 };
 
 pub fn print(_: &str, args: &List, env: &Rc<Env>) -> EvalResult {
-    for (index, expr) in args.iter().enumerate() {
-        if index > 0 {
-            print!(" ");
-        }
+    for expr in args.iter() {
         match eval(expr, env)? {
             Expr::Str(text) => print!("{}", text), // w/o double quotes
             expr => print!("{}", expr),
         }
     }
+    let _ = std::io::stdout().flush();
     Ok(NIL)
+}
+
+pub fn read(_: &str, _: &List, _: &Rc<Env>) -> EvalResult {
+    let mut input = String::new();
+    if let Err(error) = std::io::stdin().read_line(&mut input) {
+        return Err(format!("Error reading input: {}", error));
+    }
+    Ok(Expr::from(input.trim()))
+}
+
+pub fn parse_num(proc_name: &str, args: &List, env: &Rc<Env>) -> EvalResult {
+    let expr = get_exact_1_arg(proc_name, args)?;
+    let text = eval_to_str(proc_name, expr, env)?;
+
+    match text.parse::<f64>() {
+        Ok(num) => Ok(Expr::from(num)),
+        Err(_) => Err(format!("{}: '{}' is not a number", proc_name, text)),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rusp::{eval::EvalContext, list};
+
+    #[test]
+    fn test_parse_num() {
+        let context = EvalContext::new();
+        let parse_num = |args| parse_num("parse-num", &args, context.root_env());
+
+        assert_eq!(parse_num(list!("1")), Ok(Expr::from(1)));
+        assert_eq!(parse_num(list!("-24.5")), Ok(Expr::from(-24.5)));
+        assert_eq!(parse_num(list!("999.9")), Ok(Expr::from(999.9)));
+        assert_eq!(parse_num(list!("-2e12")), Ok(Expr::from(-2e12)));
+
+        assert!(parse_num(list!(1)).is_err());
+        assert!(parse_num(list!("")).is_err());
+        assert!(parse_num(list!("1", "2")).is_err());
+        assert!(parse_num(list!("xyz")).is_err());
+        assert!(parse_num(list!("1yz")).is_err());
+    }
 }
