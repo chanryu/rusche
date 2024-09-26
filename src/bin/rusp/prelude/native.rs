4c7e31f422fb758120c427d3a61f7692c90338d9
Chan Ryu <chan.ryu@safetyculture.io>
2024-09-27 09:00:56 +1000

    parse-num -> builtin:num-parse

--- src/bin/rusp/prelude/native.rs
+++ src/bin/rusp/prelude/native.rs
@@ -1,7 +1,6 @@
 use std::io::Write;
 
 use rusp::{
-    builtin::utils::{eval_to_str, get_exact_1_arg},
     eval::{eval, EvalContext, EvalResult},
     expr::{Expr, NIL},
     list::List,
@@ -24,37 +23,4 @@ pub fn read(_: &str, _: &List, _: &EvalContext) -> EvalResult {
         return Err(format!("Error reading input: {}", error));
     }
     Ok(Expr::from(input.trim()))
-}
-
-pub fn parse_num(proc_name: &str, args: &List, context: &EvalContext) -> EvalResult {
-    let expr = get_exact_1_arg(proc_name, args)?;
-    let text = eval_to_str(proc_name, expr, context)?;
-
-    match text.parse::<f64>() {
-        Ok(num) => Ok(Expr::from(num)),
-        Err(_) => Err(format!("{}: '{}' is not a number", proc_name, text)),
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-    use rusp::{eval::Evaluator, list};
-
-    #[test]
-    fn test_parse_num() {
-        let evaluator = Evaluator::with_builtin();
-        let parse_num = |args| parse_num("parse-num", &args, &evaluator.context());
-
-        assert_eq!(parse_num(list!("1")), Ok(Expr::from(1)));
-        assert_eq!(parse_num(list!("-24.5")), Ok(Expr::from(-24.5)));
-        assert_eq!(parse_num(list!("999.9")), Ok(Expr::from(999.9)));
-        assert_eq!(parse_num(list!("-2e12")), Ok(Expr::from(-2e12)));
-
-        assert!(parse_num(list!(1)).is_err());
-        assert!(parse_num(list!("")).is_err());
-        assert!(parse_num(list!("1", "2")).is_err());
-        assert!(parse_num(list!("xyz")).is_err());
-        assert!(parse_num(list!("1yz")).is_err());
-    }
 }
--- src/builtin/num.rs
+++ src/builtin/num.rs
@@ -2,7 +2,7 @@ use crate::eval::{eval, EvalContext, EvalResult};
 use crate::expr::Expr;
 use crate::list::List;
 
-use super::utils::{eval_to_num, get_exact_1_arg, get_exact_2_args};
+use super::utils::{eval_to_num, eval_to_str, get_exact_1_arg, get_exact_2_args};
 
 pub fn is_num(proc_name: &str, args: &List, context: &EvalContext) -> EvalResult {
     if let Expr::Num(_, _) = eval(get_exact_1_arg(proc_name, args)?, context)? {
@@ -56,6 +56,16 @@ pub fn modulo(proc_name: &str, args: &List, context: &EvalContext) -> EvalResult
     let rhs = eval_to_num(proc_name, rhs, context)?;
 
     Ok(Expr::Num(lhs % rhs, None))
+}
+
+pub fn parse(proc_name: &str, args: &List, context: &EvalContext) -> EvalResult {
+    let expr = get_exact_1_arg(proc_name, args)?;
+    let text = eval_to_str(proc_name, expr, context)?;
+
+    match text.parse::<f64>() {
+        Ok(num) => Ok(Expr::from(num)),
+        Err(_) => Err(format!("{}: '{}' is not a number", proc_name, text)),
+    }
 }
 
 fn logical_operation(
@@ -168,5 +178,22 @@ mod tests {
 
         // (% "1" "2") => error
         assert!(modulo("", &list!("1", "2"), context).is_err());
+    }
+
+    #[test]
+    fn test_parse() {
+        let evaluator = Evaluator::with_builtin();
+        let num_parse = |args| parse("num-parse", &args, &evaluator.context());
+
+        assert_eq!(num_parse(list!("1")), Ok(Expr::from(1)));
+        assert_eq!(num_parse(list!("-24.5")), Ok(Expr::from(-24.5)));
+        assert_eq!(num_parse(list!("999.9")), Ok(Expr::from(999.9)));
+        assert_eq!(num_parse(list!("-2e12")), Ok(Expr::from(-2e12)));
+
+        assert!(num_parse(list!(1)).is_err());
+        assert!(num_parse(list!("")).is_err());
+        assert!(num_parse(list!("1", "2")).is_err());
+        assert!(num_parse(list!("xyz")).is_err());
+        assert!(num_parse(list!("1yz")).is_err());
     }
 }
