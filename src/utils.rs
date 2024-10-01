711cb9450c295ffbcbc93db4ac97ee2554e906b6
Chan Ryu <chan.ryu@safetyculture.io>
2024-10-01 22:19:06 +1000

    Doc comments - utils.rs (#56)

--- src/utils.rs
+++ src/utils.rs
@@ -5,6 +5,7 @@ use crate::eval::{eval, EvalContext, EvalError};
 use crate::expr::{intern, Expr};
 use crate::list::{cons, List};
 
+/// Make a generic syntax error message with the given procedure name and arguments.
 pub fn make_syntax_error(proc_name: &str, args: &List) -> EvalError {
     format!(
         "Ill-formed syntax: {}",
@@ -12,6 +13,33 @@ pub fn make_syntax_error(proc_name: &str, args: &List) -> EvalError {
     )
 }
 
+/// Get exactly one argument from a list.
+///
+/// Check if `args` contains extactly one argument. If so, return a reference
+/// to the argument. Otherwise, return an error message.
+///
+/// # Arguments
+///
+/// * `proc_name` - Name of the procedure who is calling this function.
+/// * `args` - List of arguments.
+///
+/// # Example
+///
+/// ```
+/// use rusche::{
+///     expr::Expr,
+///     utils::get_exact_1_arg,
+///     list
+/// };
+///
+/// let args = list!(1);
+/// let result = get_exact_1_arg("add", &args);
+/// assert_eq!(result, Ok(&Expr::from(1)));
+///
+/// let args = list!(1, 2);
+/// let result = get_exact_1_arg("add", &args);
+/// assert!(result.is_err());
+/// ```
 pub fn get_exact_1_arg<'a>(proc_name: &str, args: &'a List) -> Result<&'a Expr, EvalError> {
     let mut iter = args.iter();
     let Some(arg) = iter.next() else {
@@ -24,6 +52,33 @@ pub fn get_exact_1_arg<'a>(proc_name: &str, args: &'a List) -> Result<&'a Expr, 
     }
 }
 
+/// Get exactly two arguments from a list.
+///
+/// Check if `args` contains extactly two arguments. If so, return a tuple that contains
+/// references to the two arguments. Otherwise, return an error message.
+///
+/// # Arguments
+///
+/// * `proc_name` - Name of the procedure who is calling this function.
+/// * `args` - List of arguments.
+///
+/// # Example
+///
+/// ```
+/// use rusche::{
+///     expr::Expr,
+///     utils::get_exact_2_args,
+///     list
+/// };
+///
+/// let args = list!(1, 2);
+/// let result = get_exact_2_args("add", &args);
+/// assert_eq!(result, Ok((&Expr::from(1), &Expr::from(2))));
+///
+/// let args = list!(1);
+/// let result = get_exact_2_args("add", &args);
+/// assert!(result.is_err());
+/// ```
 pub fn get_exact_2_args<'a>(
     proc_name: &str,
     args: &'a List,
@@ -42,7 +97,33 @@ pub fn get_exact_2_args<'a>(
     }
 }
 
-#[allow(dead_code)]
+/// Get exactly three arguments from a list.
+///
+/// Check if `args` contains extactly three arguments. If so, return a tuple that contains
+/// references to the three arguments. Otherwise, return an error message.
+///
+/// # Arguments
+///
+/// * `proc_name` - Name of the procedure who is calling this function.
+/// * `args` - List of arguments.
+///
+/// # Example
+///
+/// ```
+/// use rusche::{
+///     expr::Expr,
+///     utils::get_exact_3_args,
+///     list
+/// };
+///
+/// let args = list!(1, 2, 3);
+/// let result = get_exact_3_args("add", &args);
+/// assert_eq!(result, Ok((&Expr::from(1), &Expr::from(2), &Expr::from(3))));
+///
+/// let args = list!(1, 2, 3, 4);
+/// let result = get_exact_3_args("add", &args);
+/// assert!(result.is_err());
+/// ```
 pub fn get_exact_3_args<'a>(
     proc_name: &str,
     args: &'a List,
@@ -64,6 +145,33 @@ pub fn get_exact_3_args<'a>(
     }
 }
 
+/// Get two or three arguments from a list.
+///
+/// Check if `args` contains two or three arguments. If so, return a tuple that contains
+/// references to the two arguments and optional 3rd argument. Otherwise, return an error message.
+///
+/// # Arguments
+///
+/// * `proc_name` - Name of the procedure who is calling this function.
+/// * `args` - List of arguments.
+///
+/// # Example
+///
+/// ```
+/// use rusche::{
+///     expr::Expr,
+///     utils::get_2_or_3_args,
+///     list
+/// };
+///
+/// let args = list!(1, 2);
+/// let result = get_2_or_3_args("add", &args);
+/// assert_eq!(result, Ok((&Expr::from(1), &Expr::from(2), None)));
+///
+/// let args = list!(1, 2, 3);
+/// let result = get_2_or_3_args("add", &args);
+/// assert_eq!(result, Ok((&Expr::from(1), &Expr::from(2), Some(&Expr::from(3)))));
+/// ```
 pub fn get_2_or_3_args<'a>(
     proc_name: &str,
     args: &'a List,
@@ -85,6 +193,12 @@ pub fn get_2_or_3_args<'a>(
     }
 }
 
+/// Make a vector of symbol names from a list of arguments.
+///
+/// Check if `list` contains only symbols. If so, return a vector of the symbols.
+/// Otherwise, return an error message. This function can be used to extract formal
+/// arguments when implementing a function-like special form such as lambda or macro.
+/// ```
 pub fn make_formal_args(list: &List) -> Result<Vec<String>, EvalError> {
     let mut formal_args = Vec::new();
     for item in list.iter() {
@@ -97,6 +211,30 @@ pub fn make_formal_args(list: &List) -> Result<Vec<String>, EvalError> {
     Ok(formal_args)
 }
 
+/// Evaluate an expression into a string.
+///
+/// Check if `expr` evaluates to a string. If so, return the string. Otherwise, return an error message.
+///
+/// # Arguments
+///
+/// * `proc_name` - Name of the procedure who is calling this function.
+/// * `expr` - Expression to evaluate.
+/// * `context` - Evaluation context.
+///
+/// # Example
+///
+/// ```
+/// use rusche::{
+///     eval::Evaluator,
+///     expr::Expr,
+///     utils::eval_into_str,
+/// };
+///
+/// let evaluator = Evaluator::new();
+/// let expr = Expr::from("hello");
+/// let result = eval_into_str("test", &expr, evaluator.context());
+/// assert_eq!(result, Ok("hello".to_string()));
+/// ```
 pub fn eval_into_str(
     proc_name: &str,
     expr: &Expr,
@@ -110,6 +248,30 @@ pub fn eval_into_str(
     }
 }
 
+/// Evaluate an expression into a number (`f64``).
+///
+/// Check if `expr` evaluates to a number. If so, return the number. Otherwise, return an error message.
+///
+/// # Arguments
+///
+/// * `proc_name` - Name of the procedure who is calling this function.
+/// * `expr` - Expression to evaluate.
+/// * `context` - Evaluation context.
+///
+/// # Example
+///
+/// ```
+/// use rusche::{
+///     eval::Evaluator,
+///     expr::Expr,
+///     utils::eval_into_num,
+/// };
+///
+/// let evaluator = Evaluator::new();
+/// let expr = Expr::from(12e-3);
+/// let result = eval_into_num("test", &expr, evaluator.context());
+/// assert_eq!(result, Ok(12e-3));
+/// ```
 pub fn eval_into_num(
     proc_name: &str,
     expr: &Expr,
@@ -123,6 +285,41 @@ pub fn eval_into_num(
     }
 }
 
+/// Evaluate an expression into an integer (`i32`).
+///
+/// Check if `expr` evaluates to `f64`` with `fract() == 0``. If so, return the number
+/// as i32. Otherwise, return an error message.
+///
+/// # Arguments
+///
+/// * `proc_name` - Name of the procedure who is calling this function.
+/// * `arg_name` - Name of the argument that we want to evaluate to an integer.
+/// * `expr` - Expression to evaluate.
+/// * `context` - Evaluation context.
+///
+/// # Example
+///
+/// ```
+/// use rusche::{
+///     eval::Evaluator,
+///     expr::Expr,
+///     utils::eval_into_int,
+/// };
+///
+/// let evaluator = Evaluator::new();
+///
+/// let expr = Expr::from(12);
+/// let result = eval_into_int("test", "index", &expr, evaluator.context());
+/// assert_eq!(result, Ok(12));
+///
+/// let expr = Expr::from(12.0);
+/// let result = eval_into_int("test", "index", &expr, evaluator.context());
+/// assert_eq!(result, Ok(12));
+///
+/// let expr = Expr::from(12.5);
+/// let result = eval_into_int("test", "index", &expr, evaluator.context());
+/// assert!(result.is_err());
+/// ```
 pub fn eval_into_int(
     proc_name: &str,
     arg_name: &str,
@@ -141,6 +338,34 @@ pub fn eval_into_int(
     }
 }
 
+/// Evaluate an expression into a foreign object.
+///
+/// Check if `expr` evaluates to a foreign object (`Expr::Foreign`). If so, return
+/// the object (`Rc<dyn Any>`). Otherwise, return an error message.
+/// The caller of this function can downcase the object to the expected type.
+///
+/// # Arguments
+///
+/// * `proc_name` - Name of the procedure who is calling this function.
+/// * `expr` - Expression to evaluate.
+/// * `context` - Evaluation context.
+///
+/// # Example
+///
+/// ```
+/// use std::{any::Any, rc::Rc};
+/// use rusche::{
+///     eval::Evaluator,
+///     expr::Expr,
+///     utils::eval_into_foreign,
+/// };
+///
+/// let evaluator = Evaluator::new();
+/// let context = evaluator.context();
+/// let expr = Expr::Foreign(Rc::new(Vec::<i32>::new()));
+/// let object = eval_into_foreign("test", &expr, context).unwrap();
+/// assert!(object.downcast::<Vec<i32>>().is_ok());
+/// ```
 pub fn eval_into_foreign(
     proc_name: &str,
     expr: &Expr,
