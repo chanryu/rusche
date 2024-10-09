d91bb8812f82a2b812d8c6aaf28ac69176b0b04a
Chan Ryu <upriser@gmail.com>
2024-10-09 11:07:54 +1100

    macros.rs

---
+++ src/macros.rs
@@ -0,0 +1,16 @@
+#[macro_export]
+macro_rules! list {
+    () => {
+        $crate::list::List::Nil
+    };
+
+    ($car:literal $(, $cdr:expr)*) => {
+        $crate::list::cons($crate::expr::Expr::from($car), list!($($cdr),*))
+    };
+
+    ($car:expr $(, $cdr:expr)*) => {
+        $crate::list::cons($car, list!($($cdr),*))
+    };
+}
+
+pub(crate) use list;
