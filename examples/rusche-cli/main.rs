da105ca5e6d86ef2d3327eafb0ff416f35f642dd
Chan Ryu <chan.ryu@safetyculture.io>
2024-10-20 23:17:36 +1100

    Colorise `rusche-cli` (#86)

--- examples/rusche-cli/main.rs
+++ examples/rusche-cli/main.rs
@@ -1,7 +1,8 @@
 mod builtin;
 mod repl;
 
-use rusche::{tokenize, Evaluator, LexError, ParseError, Parser};
+use colored::Colorize;
+use rusche::{tokenize, Evaluator, LexError, Loc, ParseError, Parser, Span};
 
 use builtin::{load_io_procs, load_vec_procs};
 use repl::run_repl;
@@ -9,59 +10,113 @@ use repl::run_repl;
 fn main() {
     let mut args = std::env::args().skip(1); // skip the program name
 
-    if let Some(path) = args.next() {
-        run_file(&path);
-    } else {
-        run_repl();
-    }
-}
-
-fn run_file(path: &str) {
-    match std::fs::read_to_string(path) {
-        Ok(text) => run_file_content(&text),
-        Err(e) => eprintln!("Failed to read file at \"{path}\": {e}"),
-    }
-}
-
-fn run_file_content(text: &str) {
-    let tokens = match tokenize(text, None) {
-        Ok(tokens) => tokens,
-        Err(LexError::IncompleteString(span)) => {
-            eprintln!("Error:{span}: Incomplete string");
-            return;
-        }
-        Err(LexError::InvalidNumber(span)) => {
-            eprintln!("Error:{span}: Invalid number");
-            return;
-        }
-    };
-
     let evaluator = Evaluator::with_prelude();
 
     load_io_procs(evaluator.context());
     load_vec_procs(evaluator.context());
 
-    let mut parser = Parser::with_tokens(tokens);
-    loop {
-        match parser.parse() {
-            Ok(None) => {
-                break;
-            }
-            Ok(Some(expr)) => match evaluator.eval(&expr) {
-                Ok(_) => {}
-                Err(e) => match e.span {
-                    Some(span) => eprintln!("Error: {}:{}", span, e.message),
-                    None => eprintln!("Error: {}", e.message),
+    if let Some(path) = args.next() {
+        run_file(evaluator, &path);
+    } else {
+        run_repl(evaluator);
+    }
+}
+
+fn run_file(evaluator: Evaluator, path: &str) {
+    match std::fs::read_to_string(path) {
+        Ok(text) => {
+            let tokens = match tokenize(&text, None) {
+                Ok(tokens) => tokens,
+                Err(error) => match error {
+                    LexError::InvalidNumber(span) => {
+                        print_error("invalid number", &text, Some(span));
+                        return;
+                    }
+                    LexError::IncompleteString(span) => {
+                        print_error("incomplete string", &text, Some(span));
+                        return;
+                    }
                 },
-            },
-            Err(ParseError::IncompleteExpr(_)) => {
-                eprintln!("Failed to parse - incomplete expression");
-                break;
+            };
+
+            let mut parser = Parser::with_tokens(tokens);
+            loop {
+                match parser.parse() {
+                    Ok(None) => {
+                        break;
+                    }
+                    Ok(Some(expr)) => match evaluator.eval(&expr) {
+                        Ok(_) => {}
+                        Err(e) => {
+                            print_error(&e.message, &text, e.span);
+                            break;
+                        }
+                    },
+                    Err(ParseError::IncompleteExpr(token)) => {
+                        let begin_loc = token.span().begin;
+                        let end_loc =
+                            Loc::new(text.lines().count() - 1, text.lines().last().unwrap().len());
+                        print_error(
+                            "incomplete expression",
+                            &text,
+                            Some(Span::new(begin_loc, end_loc)),
+                        );
+                        break;
+                    }
+                    Err(ParseError::UnexpectedToken(token)) => {
+                        print_error(
+                            &format!("unexpected token: \"{token}\""),
+                            &text,
+                            Some(token.span()),
+                        );
+                        break;
+                    }
+                }
             }
-            Err(ParseError::UnexpectedToken(token)) => {
-                eprintln!("Error: Unexpected token - {token}");
-                break;
-            }
+        }
+        Err(e) => eprintln!("Failed to read file at \"{path}\": {e}"),
+    }
+}
+
+fn print_error(message: &str, src: &str, span: Option<Span>) {
+    let lines: Vec<&str> = src.lines().collect();
+
+    println!("{}: {}", "error".red(), message);
+
+    let Some(span) = span else { return };
+
+    if span.end.line < lines.len() {
+        let print_line =
+            |line| println!("{}{}", format!("{:>3}| ", line + 1).dimmed(), lines[line]);
+        if span.begin.line >= 2 {
+            print_line(span.begin.line - 2);
+        }
+        if span.begin.line >= 1 {
+            print_line(span.begin.line - 1);
+        }
+
+        for line in span.begin.line..span.end.line + 1 {
+            print_line(line);
+
+            let begin_col = if line == span.begin.line {
+                span.begin.column
+            } else {
+                lines[line]
+                    .chars()
+                    .take_while(|c| c.is_whitespace())
+                    .count()
+            };
+            let end_col = if line == span.end.line {
+                span.end.column
+            } else {
+                lines[line].len()
+            };
+            println!(
+                "{}{}{}",
+                "   | ".dimmed(),
+                " ".repeat(begin_col),
+                "^".repeat(end_col - begin_col).red()
+            );
         }
     }
 }
--- examples/rusche-cli/repl.rs
+++ examples/rusche-cli/repl.rs
@@ -1,75 +1,74 @@
-use rusche::{tokenize, Evaluator, LexError, Loc, ParseError, Parser, Span};
+use colored::Colorize;
+use rusche::{tokenize, Evaluator, LexError, Loc, ParseError, Parser};
 use rustyline::{error::ReadlineError, DefaultEditor};
 
-use crate::builtin::{load_io_procs, load_vec_procs};
+use crate::print_error;
 
-pub fn run_repl() {
+pub fn run_repl(evaluator: Evaluator) {
     print_logo();
 
     let mut rl = DefaultEditor::new().expect("Failed to initialize line reader!");
 
-    let evaluator = Evaluator::with_prelude();
-
-    load_io_procs(evaluator.context());
-    load_vec_procs(evaluator.context());
-
-    let mut lines = Vec::new();
+    let mut src = String::new();
 
     let mut parser = Parser::new();
     loop {
-        let prompt = if parser.is_parsing() {
-            &format!("{:06} ❯ ", lines.len() + 1)
+        let line = src.lines().count();
+        let prompt = if line == 0 {
+            format!("rusche:{:02}❯ ", line + 1)
         } else {
-            "rusche ❯ "
+            debug_assert!(parser.is_parsing());
+            format!("......:{:02}❯ ", line + 1)
         };
 
-        match rl.readline(prompt) {
-            Ok(line) => {
-                let _ = rl.add_history_entry(line.as_str());
-                let loc = Some(Loc::new(lines.len(), 0));
-                let res = tokenize(&line, loc);
-
-                lines.push(line);
+        match rl.readline(&prompt) {
+            Ok(text) => {
+                let _ = rl.add_history_entry(text.as_str());
+                let loc = Some(Loc::new(line, 0));
+                let res = tokenize(&text, loc);
 
                 match res {
                     Ok(tokens) => parser.add_tokens(tokens),
                     Err(err) => {
+                        let error_src = src.clone() + &text;
                         match err {
                             LexError::InvalidNumber(span) => {
-                                print_error("invalid number", &lines, Some(span))
+                                print_error("invalid number", &error_src, Some(span))
                             }
                             LexError::IncompleteString(span) => {
-                                print_error("incomplete string", &lines, Some(span))
+                                print_error("incomplete string", &error_src, Some(span))
                             }
                         }
-                        lines.pop();
                         continue;
                     }
                 }
 
+                src.push_str(&text);
+                src.push_str("\n");
+
                 loop {
                     match parser.parse() {
                         Ok(None) => {
-                            lines.clear();
+                            src.clear();
                             break;
                         }
                         Ok(Some(expr)) => match evaluator.eval(&expr) {
                             Ok(result) => {
-                                println!("{}", result);
+                                println!("{}", result.to_string().green());
                             }
                             Err(error) => {
-                                print_error(&error.message, &lines, error.span);
+                                print_error(&error.message, &src, error.span);
                             }
                         },
                         Err(ParseError::IncompleteExpr(_)) => break,
                         Err(ParseError::UnexpectedToken(token)) => {
                             parser.reset();
                             print_error(
-                                &format!("unexpected token - {token}"),
-                                &lines,
+                                &format!("unexpected token: \"{token}\""),
+                                &src,
                                 Some(token.span()),
                             );
-                            lines.clear();
+                            src.clear();
                         }
                     }
                 }
@@ -85,32 +84,13 @@ pub fn run_repl() {
     }
 }
 
+#[rustfmt::skip]
 fn print_logo() {
-    println!(r"              ____                  __         ");
-    println!(r"             / __ \__  ____________/ /_  ___   ");
-    println!(r"            / /_/ / / / / ___/ ___/ __ \/ _ \  ");
-    println!(r"Welcome to / _, _/ /_/ (__  ) /__/ / / /  __/ !");
-    println!(r"          /_/ |_|\__,_/____/\___/_/ /_/\___/   ");
-    println!(r"                                               ");
-    println!(r"To exit, press Ctrl + D.                       ");
-}
+    println!("          {}  ", r"    ____                  __       ".bold().cyan());
+    println!("          {}  ", r"   / __ \__  ____________/ /_  ___ ".bold().cyan());
+    println!("          {}  ", r"  / /_/ / / / / ___/ ___/ __ \/ _ \".bold().cyan());
+    println!("Welcome to{} !", r" / _, _/ /_/ (__  ) /__/ / / /  __/".bold().cyan());
+    println!("          {}  ", r"/_/ |_|\__,_/____/\___/_/ /_/\___/ ".bold().cyan());
 
-fn print_error(message: &str, lines: &Vec<String>, span: Option<Span>) {
-    if let Some(span) = span {
-        println!("error [{span}]: {message}");
-
-        if span.begin.line < lines.len() {
-            if span.begin.line > 0 {
-                println!("{:03}: {}", span.begin.line, lines[span.begin.line - 1]);
-            }
-            println!("{:03}: {}", span.begin.line + 1, lines[span.begin.line]);
-            println!(
-                "     {}{}",
-                " ".repeat(span.begin.column),
-                "^".repeat(span.end.column - span.begin.column)
-            );
-        }
-    } else {
-        println!("error: {message}");
-    }
+    println!("\n{}", "To exit, press Ctrl + D.".dimmed());
 }
